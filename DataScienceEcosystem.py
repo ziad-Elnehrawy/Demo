9604b6bec540a8c4f1a2fad733a21d187de7f805
ziad-Elnehrawy <134233486+ziad-Elnehrawy@users.noreply.github.com>
2023-05-23 15:11:28 +0300

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,171 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "d7302a1f-137d-467e-a5ff-6726fcc56822",
+   "metadata": {},
+   "source": [
+    "## Author\n",
+    "My name is [Ziad Ahmed Elnehrawy]."
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "a1193861-de25-464e-bd49-e2f308c7b8e4",
+   "metadata": {},
+   "source": [
+    "# Data Science Tools and Ecosystem"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "753e89ad-79c3-4e0d-8260-dac15fbfcdd9",
+   "metadata": {
+    "tags": []
+   },
+   "source": [
+    "# In this notebook, Data Science Tools and Ecosystem are summarized."
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "75dfc658-595c-44fe-98ae-1476000e521e",
+   "metadata": {
+    "jp-MarkdownHeadingCollapsed": true,
+    "tags": []
+   },
+   "source": [
+    "# Objectives:\n",
+    "\n",
+    "- List popular languages for Data Science\n",
+    "- Identify commonly used libraries and frameworks in Data Science\n",
+    "- Highlight the importance of development environments such as Jupyter Notebook, RStudio, and Spyder\n",
+    "- Demonstrate simple operations in Python for evaluating arithmetic expressions\n",
+    "- Convert units of time (minutes to hours) using Python expressions"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "73c2883f-90ea-4088-96af-6b7ef1940725",
+   "metadata": {},
+   "source": [
+    "#some of the popular languages that data Scientists use are: \n",
+    "1. Python\n",
+    "2. R\n",
+    "3. SQL"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "5cfb1a62-34c1-4513-926f-a035b14da867",
+   "metadata": {},
+   "source": [
+    "# Some of the commonly used libraries used by Data Scientists include:\n",
+    "1. pandas\n",
+    "2. NumPy\n",
+    "3. Matplotlib\n",
+    "4. Scikit-learn"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "4c9251db-3081-4aa3-87d5-47a1c129f9ed",
+   "metadata": {},
+   "source": [
+    "| Data Science Tools |\n",
+    "| ------------------ |\n",
+    "| Jupyter Notebook    |\n",
+    "| RStudio             |\n",
+    "| Spyder              |"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "fae3b45e-d003-41e2-972f-0bb8e5383f39",
+   "metadata": {},
+   "source": [
+    "### Below are a few examples of evaluating arithmetic expressions in Python:\n",
+    "1. +\n",
+    "2. -\n",
+    "3. /"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "c3a61361-b738-4b8c-b070-3f4252df2d23",
+   "metadata": {
+    "tags": []
+   },
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "17"
+      ]
+     },
+     "execution_count": 2,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "# This a simple arithmetic expression to mutiply then add integers\n",
+    "(3*4)+5"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "f9226660-0f56-432f-a5dd-41bc699dbaf5",
+   "metadata": {
+    "tags": []
+   },
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "3.3333333333333335"
+      ]
+     },
+     "execution_count": 3,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "# This will convert 200 minutes to hours by diving by 60.\n",
+    "200/60"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "8ebe58bb-6952-4b8d-ae55-67415883a095",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python",
+   "language": "python",
+   "name": "conda-env-python-py"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
